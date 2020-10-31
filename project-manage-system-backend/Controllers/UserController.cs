a8c02b4f6383f16bdadba4a2a00900fd9c1cf81b
weiChen <asdzxc55888@gmail.com>
2020-11-01 00:24:50 +0800

    Initial commit

---
+++ project-manage-system-backend/Controllers/UserController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using project_manage_system_backend.Model;
+
+namespace project_manage_system_backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetUser()
+        {
+            return Ok(new UserModel
+            {
+                ID=1,
+                Name="王八蛋",
+                PlatformName="kingEightEgg"
+            });
+        }
+    }
+}
---
+++ project-manage-system-backend/Model/UserModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace project_manage_system_backend.Model
+{
+    public class UserModel
+    {
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+
+        public string PlatformName { get; set; }
+    }
+}
