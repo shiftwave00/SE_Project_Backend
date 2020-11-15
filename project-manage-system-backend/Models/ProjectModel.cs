73a57cf4712af2ea40be52ba14dd004cfc1ffa81
zxjte9411 <a5852241@gmail.com>
2020-11-15 22:15:37 +0800

    add Proj and Repo Model

---
+++ project-manage-system-backend/Models/ProjectModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using project_manage_system_backend.Models;
+
+namespace project_manage_system_backend.Models
+{
+    public class ProjectModel
+    {
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+
+        public UserModel User { get; set; }
+
+        public List<UserModel> Members { get; set; }
+    }
+}
---
+++ project-manage-system-backend/Models/RepositoryModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using project_manage_system_backend.Models;
+
+namespace project_manage_system_backend.Models
+{
+    public class RepositoryModel
+    {
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+
+        public string Url { get; set; }
+
+        public UserModel Owner { get; set; }
+
+        public ProjectModel Project { get; set; }
+    }
+}
