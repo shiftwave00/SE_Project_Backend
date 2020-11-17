d064692ccab5f017a11353592e7750c411dd5934
weiChen <asdzxc55888@gmail.com>
2020-11-17 17:17:24 +0800

    soloved CORS problem

--- project-manage-system-backend/Startup.cs
+++ project-manage-system-backend/Startup.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -10,6 +12,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
+using project_manage_system_backend.Shares;
 
 namespace project_manage_system_backend
 {
@@ -22,22 +26,72 @@ namespace project_manage_system_backend
 
         public IConfiguration Configuration { get; }
 
+        private readonly string _developmentAllowOrigin = "http://localhost:8080";
+        private readonly string _deployAllowOrigin = "localhost:8080";
+
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
-
-            services.AddCors(c =>
-            {
-                c.AddPolicy("AllowOrigin", option => option.AllowAnyOrigin());
-            });
-
             services.AddLogging(builder =>
             {
                 builder.AddConfiguration(Configuration.GetSection("Logging"))
                        .AddConsole()
                        .AddDebug();
             });
+
+            services.AddSingleton<JwtHelper>();
+
+            services
+            .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(options =>
+            {
+                options.IncludeErrorDetails = true; // 預設值為 true，有時會特別關閉
+
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    // 透過這項宣告，就可以從 "sub" 取值並設定給 User.Identity.Name
+                    NameClaimType = "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier",
+                    // 透過這項宣告，就可以從 "roles" 取值，並可讓 [Authorize] 判斷角色
+                    RoleClaimType = "http://schemas.microsoft.com/ws/2008/06/identity/claims/role",
+
+                    // 一般我們都會驗證 Issuer
+                    ValidateIssuer = true,
+                    ValidIssuer = Configuration.GetValue<string>("JwtSettings:Issuer"),
+
+                    // 通常不太需要驗證 Audience
+                    ValidateAudience = false,
+
+                    // 一般我們都會驗證 Token 的有效期間
+                    ValidateLifetime = true,
+
+                    // 如果 Token 中包含 key 才需要驗證，一般都只有簽章而已
+                    ValidateIssuerSigningKey = false,
+
+                    // "1234567890123456" 應該從 IConfiguration 取得
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration.GetValue<string>("JwtSettings:SignKey")))
+                };
+            });
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy("DevelopmentPolicy", policy =>
+                {
+                    policy.WithOrigins(_developmentAllowOrigin)
+                          .AllowAnyHeader()
+                          .AllowAnyMethod()
+                          .AllowCredentials();
+                });
+
+                options.AddPolicy("DeployPolicy", policy =>
+                {
+                    policy.WithOrigins(_deployAllowOrigin)
+                          .AllowAnyHeader()
+                          .AllowAnyMethod()
+                          .AllowCredentials();
+                });
+            });
+
+            services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -56,12 +110,18 @@ namespace project_manage_system_backend
 
             app.UseRouting();
 
-            app.UseAuthorization();
-
-            app.UseCors(option =>
+            if (env.IsDevelopment())
             {
-                option.AllowAnyOrigin();
-            });
+                app.UseCors("DevelopmentPolicy");
+            }
+            else
+            {
+                app.UseCors("DeployPolicy");
+            }
+
+            app.UseAuthentication();
+
+            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
