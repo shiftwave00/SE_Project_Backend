28f7aeec877ad8e5299b0fbb5832c651caf33e2c
zxjte9411 <a5852241@gmail.com>
2020-11-18 12:24:38 +0800

    project controller and dto

---
+++ project-manage-system-backend/Controllers/ProjectController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using project_manage_system_backend.Dtos;
+using project_manage_system_backend.Models;
+using project_manage_system_backend.Services;
+
+namespace project_manage_system_backend.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ProjectController : ControllerBase
+    {
+        private readonly ProjectService _projectService;
+
+        public ProjectController()
+        {
+            _projectService = new ProjectService();
+        }
+
+        [HttpPost]
+        public IActionResult AddProject(ProjectDto projectDto)
+        {
+            _projectService.Create(projectDto);
+            return Ok();
+        }
+
+        [HttpGet("{encryptUserId}")]
+        public IActionResult GetProject(string encryptUserId)
+        {
+            var result = _projectService.GetProjectByUserAccount(encryptUserId);
+            return Ok(result);
+        }
+    }
+}
---
+++ project-manage-system-backend/Dtos/ProjectDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using project_manage_system_backend.Models;
+
+namespace project_manage_system_backend.Dtos
+{
+    public class ProjectDto
+    {
+        public string ProjectName { set; get; }
+        public string UserId { set; get; }
+    }
+}
