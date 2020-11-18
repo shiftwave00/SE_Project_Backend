1e547a9a2024f9242b2b4f85b3ef64ad0bc8f2df
zxjte9411 <a5852241@gmail.com>
2020-11-18 12:25:55 +0800

    migration files

---
+++ project-manage-system-backend/Migrations/20201117200337_EditDataModel.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace project_manage_system_backend.Migrations
+{
+    public partial class EditDataModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
