c6758183f68c759e863a708e06474c69af71dc4c
zxjte9411 <a5852241@gmail.com>
2020-11-19 16:25:27 +0800

    repository controller and service

---
+++ project-manage-system-backend/Services/RepositoryService.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using project_manage_system_backend.Models;
+using project_manage_system_backend.Shares;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace project_manage_system_backend.Services
+{
+    public class RepositoryService
+    {
+        public List<RepositoryModel> GetRepositoryByProjectId(int id)
+        {
+            using (var dbContent = new PMSContext())
+            {
+                var project= dbContent.Projects.Include(p => p.Repositories).FirstOrDefault(p => p.ID.Equals(id));
+                //var query = (from r in project.Repositories
+                //             select new { Id = r.ID, Name = r.Name }).ToList();
+                return project.Repositories;
+            }
+        }
+    }
+}
