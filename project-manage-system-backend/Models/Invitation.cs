daf2a14c3aa7a1fa29d5cb146fe51ec69531d26f
Eric <t105590021@ntut.org.tw>
2020-11-24 01:49:31 +0800

    add model

---
+++ project-manage-system-backend/Models/Invitation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace project_manage_system_backend.Models
+{
+    public class Invitation
+    {
+        public int ID { get; set; }
+        public User Inviter { get; set; }
+        public User Applicant { get; set; }
+        public Project InvitedProject { get; set; }
+        public bool IsAgreed { get; set; }
+    }
+}
