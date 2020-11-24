be97b8ea3ce6bcba462bbcfeb8327a3eecaf34b2
zxjte9411 <a5852241@gmail.com>
2020-11-25 01:32:09 +0800

    取得 user 資訊，name 及 avatarUrl

---
+++ project-manage-system-backend/Dtos/UserInfoDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace project_manage_system_backend.Dtos
+{
+    public class UserInfoDto
+    {
+        public string Name { set; get; }
+        public string AvatarUrl { set; get; }
+    }
+}
