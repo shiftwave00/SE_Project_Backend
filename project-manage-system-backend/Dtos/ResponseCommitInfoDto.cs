eb8023d0328aef9787881d388df6481146eada7b
weiChen <asdzxc55888@gmail.com>
2020-11-25 16:26:42 +0800

    added repoInfo api and finished get commitInfo

---
+++ project-manage-system-backend/Dtos/ResponseCommitInfoDto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace project_manage_system_backend.Dtos
+{
+    public class CommitInfo
+    {
+        public int Week { get; set; }
+
+        public int[] Days { get; set; }
+
+        public int Total { get; set; }
+    }
+
+    public class ResponseCommitInfoDto
+    {
+        public int week { get; set; }
+
+        public int[] days { get; set; }
+
+        public int total { get; set; }
+    }
+}
---
+++ project-manage-system-backend/Services/RepoInfoService.cs
@@ -0,0 +1,79 @@
+﻿using project_manage_system_backend.Dtos;
+using project_manage_system_backend.Models;
+using project_manage_system_backend.Shares;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace project_manage_system_backend.Services
+{
+    public class RepoInfoService:BaseService
+    {
+        private readonly HttpClient _httpClient;
+        public RepoInfoService(PMSContext dbContext, HttpClient client = null) :base(dbContext) 
+        {
+            _httpClient = client == null ? new HttpClient() : client;
+        }
+
+        public async Task<CommitInfoDto> RequestCommitInfo(int repoId)
+        {   
+            Repo repo = _dbContext.Repositories.Find(repoId);
+            string url = "https://api.github.com/repos/" + repo.Owner + "/" + repo.Name + "/stats/commit_activity";
+
+            _httpClient.DefaultRequestHeaders.Add("User-Agent", "request");
+
+            var response = await _httpClient.GetAsync(url);
+            string content = await response.Content.ReadAsStringAsync();
+            var commitInfos = JsonSerializer.Deserialize<List<ResponseCommitInfoDto>>(content);
+
+            List<WeekTotalData> weekChartDatas = new List<WeekTotalData>();
+            List<DayOfWeekData> detailChartDatas = new List<DayOfWeekData>();
+
+            foreach (var commitInfo in commitInfos)
+            {
+                weekChartDatas.Add(ConvertToWeekChartData(commitInfo));
+                detailChartDatas.Add(ConvertToDetailChartData(commitInfo));
+            }
+
+            return new CommitInfoDto
+            {
+                WeekTotalData = weekChartDatas,
+                DayOfWeekData = detailChartDatas
+            };
+        }
+
+        private WeekTotalData ConvertToWeekChartData(ResponseCommitInfoDto commitInfo)
+        {
+            return new WeekTotalData
+            {
+                Total = commitInfo.total,
+                Week = DateHandler.ConvertToDateString(commitInfo.week)
+            };
+        }
+
+        private DayOfWeekData ConvertToDetailChartData(ResponseCommitInfoDto commitInfo)
+        {
+            List<Dtos.DayOfWeek> detailDatas = new List<Dtos.DayOfWeek>();
+
+            int dayOfWeekCount = 0;
+            foreach (var day in commitInfo.days)
+            {
+                detailDatas.Add(new Dtos.DayOfWeek
+                {
+                    Day = DateHandler.ConvertToDayOfWeek(dayOfWeekCount++),
+                    Commit = day
+                });
+            }
+
+            return new DayOfWeekData
+            {
+                Week = DateHandler.ConvertToDateString(commitInfo.week),
+                DetailDatas = detailDatas
+            };
+        }
+    }
+}
