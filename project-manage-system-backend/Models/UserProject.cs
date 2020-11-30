96ad625894cca2508823d21106c87056ec6e428e
zxjte9411 <a5852241@gmail.com>
2020-11-30 15:27:27 +0800

    new model

---
+++ project-manage-system-backend/Models/UserProject.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace project_manage_system_backend.Models
+{
+    public class UserProject
+    {
+        public string Account { get; set; }
+        public User User { get; set; }
+        public int ProjectId { get; set; }
+        public Project Project { get; set; }
+    }
+}
